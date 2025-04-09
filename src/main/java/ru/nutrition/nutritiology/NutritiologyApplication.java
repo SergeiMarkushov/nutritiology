44c496fc023c1e58cdbf3b1952de1579bf818fa9
SeregaMarkush <s.markushov@icloud.com>
2025-04-09 18:51:35 +0300

    init

---
+++ src/main/java/ru/nutrition/nutritiology/NutritiologyApplication.java
@@ -0,0 +1,13 @@
+package ru.nutrition.nutritiology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutritiologyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NutritiologyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/nutrition/nutritiology/NutritiologyApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nutrition.nutritiology;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutritiologyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
